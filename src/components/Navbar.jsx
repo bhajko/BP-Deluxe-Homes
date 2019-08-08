80174307656c58066d9dc4da4a38980e0bcff859
bhajko <bhajko@gmail.com>
2019-08-08 22:25:04 +0200

    logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { TiThMenu } from "react-icons/ti";
+import logo from "../images/logo.svg";
 
 class Navbar extends Component {
   state = {
@@ -15,24 +16,28 @@ class Navbar extends Component {
       <nav className="navbar">
         <div className="nav-center">
           <div className="nav-header">
-            <Link to="/">BP Deluxe Homes</Link>
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
             <button type="button" className="nav-btn" onClick={this.toggle}>
               <TiThMenu className="nav-icon" />
             </button>
           </div>
-          <ul
-            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
-          >
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/apartments">Apartments</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
+          <div>
+            <ul
+              className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/apartments">Apartments</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
