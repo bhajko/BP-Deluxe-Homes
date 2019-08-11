d1b112c3f007672e6a1984bf7c3c7fad3a149dbd
bhajko <bhajko@gmail.com>
2019-08-11 19:12:25 +0200

    contact page content added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { IconContext } from "react-icons";
+import { FaMapMarker, FaPhone, FaAt } from "react-icons/fa";
 
 const Contact = () => {
-  return <div>hello from contact page</div>;
+  return (
+    <>
+      <Hero hero="contactHero">
+        <Banner title="Contact us">
+          <Link to="/" className="btn-primary">
+            Return home
+          </Link>
+        </Banner>
+      </Hero>
+      <div className="contact-wrapper">
+        <div className="map">
+          <p>ide jön a map</p>
+        </div>
+        <div className="contact-details">
+          <div className="contact-details-inner">
+            <IconContext.Provider
+              value={{
+                color: "#bf9b30",
+                size: "1.5em",
+                className: "contactIcon"
+              }}
+            >
+              <FaPhone />
+            </IconContext.Provider>
+            <p>+36 20 111 222 3</p>
+          </div>
+          <div className="contact-details-inner">
+            <IconContext.Provider
+              value={{
+                color: "#bf9b30",
+                size: "1.5em",
+                className: "contactIcon"
+              }}
+            >
+              <FaAt />
+            </IconContext.Provider>
+            <p>info@bpdeluxehomes.com</p>
+          </div>
+          <div className="contact-details-inner">
+            <IconContext.Provider
+              value={{
+                color: "#bf9b30",
+                size: "1.5em",
+                className: "contactIcon"
+              }}
+            >
+              <FaMapMarker />
+            </IconContext.Provider>
+            <p>1051 Budapest Alkotmány utca 1.</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
